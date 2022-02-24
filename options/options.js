f3df3ae9f85bfa8bce915a3681c7f0a3290cadab
Crowdbotics <team@crowdbotics.com>
2022-02-24 17:48:46 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "lamar_genomics_33826",
+  url: "https://lamar_genomics_33826.botics.co",
+  api: "https://lamar_genomics_33826.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
